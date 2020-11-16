35942a22239397ae8b7ed817719397571308fb90
vocart <vocart@gmail.com>
2020-11-16 20:28:05 +0100

    Second commit

---
+++ backup_locations.py
@@ -0,0 +1,36 @@
+# list of locations to backup with their backup directories for copy_swap()
+
+locations_copy_replace = {
+	'E:/fotki pulpit': '//192.168.20.20/kopia pulpit/Zdjęcia z pulpitu',
+
+	'C:/Users/Lech/Desktop/Programowanie': '//192.168.20.20/kopia pulpit/Programowanie',
+	
+	'C:/Zdjęcia do obróbki': '//192.168.20.20/kopia pulpit/Zdjęcia z pulpitu/Zdjęcia do obróbki',
+
+	'H:/muza/GRAJEK': '//192.168.20.20/muzyka/GRAJEK',
+	'H:/muza/REDMI': '//192.168.20.20/muzyka/REDMI',
+	
+	'H:/MOJE/Warhammer i gry': '//192.168.20.20/kopia pulpit/Warhammer i gry',
+	'H:/MOJE/Zdjęcia': '//192.168.20.20/zdjęcia',
+	'H:/MOJE/Muzyka/mp3': '//192.168.20.20/muzyka/mp3',
+
+	'H:/MOJE/Różne/PRACA/FIRMA': '//192.168.20.20/kopia pulpit/PRACA/FIRMA',
+	'H:/MOJE/Różne/PRACA/ARCHITEKTURA': '//192.168.20.20/kopia pulpit/PRACA/ARCHITEKTURA',
+	'H:/MOJE/Różne/PRACA/CV': '//192.168.20.20/kopia pulpit/PRACA/CV',
+	'H:/MOJE/Różne/PRACA/Pity': '//192.168.20.20/kopia pulpit/PRACA/Pity',
+
+	'H:/MOJE/Różne/DOM': '//192.168.20.20/kopia pulpit/DOM',
+	'H:/MOJE/Różne/PROGRAMY': '//192.168.20.20/kopia pulpit/PROGRAMY',
+	'H:/MOJE/Różne/RESZTA': '//192.168.20.20/kopia pulpit/RESZTA',
+	'H:/MOJE/Różne/ROZRYWKA': '//192.168.20.20/kopia pulpit/ROZRYWKA'
+	}
+
+
+# list of files to copy for copy_new_version()
+
+locations_copy_new_version = (
+	'H:/MOJE/figurki - aukcje, kasa.xls', 
+	'H:/MOJE/obiektywy.xls',
+	'H:/MOJE/figurki - spis 2020.xls',
+	'H:/MOJE/waga.xls'
+	)
